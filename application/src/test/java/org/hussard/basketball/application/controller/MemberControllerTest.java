79728a54dd741ebf9abd2afd4a18624ae783f116
stephane <stephane.betton@gmail.com>
2024-01-03 18:45:24 +0100

    feat: GetMember
    Add init TDD test

---
+++ application/src/test/java/org/hussard/basketball/application/controller/MemberControllerTest.java
@@ -0,0 +1,14 @@
+package org.hussard.basketball.application.controller;
+
+import org.junit.jupiter.api.Test;
+
+class MemberControllerTest {
+    @Test
+    void getMember_shouldReturnMemberById(){
+        // todo
+    }
+    @Test
+    void getMember_shouldReturnNotFoundIfMemberDoesNotExist() throws Exception {
+        // todo
+    }
+}
---
+++ application/src/test/java/org/hussard/basketball/application/service/MemberServiceTest.java
@@ -0,0 +1,14 @@
+package org.hussard.basketball.application.service;
+
+import org.junit.jupiter.api.Test;
+
+public class MemberServiceTest {
+    @Test
+    void getMember_shouldReturnMemberById(){
+        // todo
+    }
+    @Test
+    void getMember_shouldReturnExceptionIfMemberDoesNotExist() throws Exception {
+        // todo
+    }
+}
